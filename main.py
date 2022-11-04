18099e6227ca76a4c057ac6f896a5832a508f7fd
rudalson <rudalson@gmail.com>
2022-11-04 19:34:00 +0900

    ✅ added : rest api get/post about blog

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,6 +10,40 @@ def index():
     return {'data': {'name': 'kmpak'}}
 
 
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs list'}
+    else:
+        return {'data': f'{limit}  blogs list'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit=10):
+    return {'data': {'1', '2'}}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
 @app.get('/about')
 def about():
     return {'data': 'about page'}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f"Blog is created with title as {blog.title}"}
