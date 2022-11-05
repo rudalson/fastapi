4f25a66d1f6f49afb09748f2a7206773f75e8427
rudalson <rudalson@gmail.com>
2022-11-05 18:48:32 +0900

    ✅ added : main 실행. readme.md 추가

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
@@ -47,3 +48,7 @@ class Blog(BaseModel):
 @app.post('/blog')
 def create_blog(blog: Blog):
     return {'data': f"Blog is created with title as {blog.title}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='127.0.0.1', port='9000')
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Fastapi Toy Project
+
+간단한 blog backend 구현해보기
+
+## Requirements
+
+```
+pip install fastapi
+pip install uvicorn
+```
+
+## Execution
+
+```
+uvicorn main:app --reload
+```
+
+or
+
+```
+python main.py
+```
