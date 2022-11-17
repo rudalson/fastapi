2499fef7acb7a3482ed7c27426a9b4e6de090278
rudalson <rudalson@gmail.com>
2022-11-17 19:43:17 +0900

    ✅ added : crud function to repository

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -1,8 +1,8 @@
 from typing import List
 from fastapi import APIRouter, Depends, status, Response, HTTPException
 from .. import schemas, database, models
-from ..hashing import Hash
 from sqlalchemy.orm import Session
+from .. repository import user
 
 
 router = APIRouter(
@@ -14,26 +14,14 @@ get_db = database.get_db
 
 @router.post('', response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    new_user = models.User(
-        name=request.name, email=request.email, password=Hash.bcrypt(request.password))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+    return user.create(request, db)
 
 
 @router.get('/{id}', response_model=schemas.ShowUser)
 def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"User with the id {id} is not available")
-
-    return user
+    return user.get(id, db)
 
 
 @router.get('', response_model=List[schemas.ShowUser])
 def all(db: Session = Depends(get_db)):
-    users = db.query(models.User).all()
-    return users
+    return user.get_all(db)
