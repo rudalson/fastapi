a148ef76a56a51aefd2d7abbcc477bf9c3fcf8af
rudalson <rudalson@gmail.com>
2022-11-18 18:49:18 +0900

    ✅ added: jwt token생성

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException
-from .. import schemas, database, models
+from .. import schemas, database, models, token
 from ..hashing import Hash
 from sqlalchemy.orm import Session
 
@@ -19,5 +19,9 @@ def login(request: schemas.Login, db: Session = Depends(database.get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Incorrect password")
 
-    # todo : generate a JWT token and return
-    return user
+    # todo : generate a JWT token and
+    access_token = token.create_access_token(data={"sub": user.email})
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+    # return user
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 from pydantic import BaseModel
 
 
@@ -39,3 +39,12 @@ class ShowBlog(BaseModel):
 class Login(BaseModel):
     username: str
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
