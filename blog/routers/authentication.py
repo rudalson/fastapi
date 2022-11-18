7509494533ab2dc9471888c74dc9480ba5b65a03
rudalson <rudalson@gmail.com>
2022-11-18 19:42:14 +0900

    ✅ added: login and 인증 적용

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from .. import schemas, database, models, token
 from ..hashing import Hash
 from sqlalchemy.orm import Session
@@ -7,7 +8,7 @@ router = APIRouter(tags=['Authentication'])
 
 
 @router.post('/login')
-def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(
         models.User.email == request.username).first()
 
@@ -19,7 +20,6 @@ def login(request: schemas.Login, db: Session = Depends(database.get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Incorrect password")
 
-    # todo : generate a JWT token and
     access_token = token.create_access_token(data={"sub": user.email})
 
     return {"access_token": access_token, "token_type": "bearer"}
--- blog/schemas.py
+++ blog/schemas.py
@@ -47,4 +47,4 @@ class Token(BaseModel):
 
 
 class TokenData(BaseModel):
-    username: Union[str, None] = None
+    email: Union[str, None] = None
