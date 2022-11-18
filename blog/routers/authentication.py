89169115f3a2fb99b3ed2d584c23b098c8badee2
rudalson <rudalson@gmail.com>
2022-11-18 10:17:55 +0900

    🐛 modified : 로긴 유저 조회시 무한 string으로 뻗는 문제 수정

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -5,13 +5,12 @@ from sqlalchemy.orm import Session
 router = APIRouter(tags=['Authentication'])
 
 
-@router.post('/login', response_model=schemas.User)
+@router.post('/login')
 def login(request: schemas.Login, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == request.username)
+    user = db.query(models.User).filter(
+        models.User.email == request.username).first()
 
-    print(user.first())
-
-    if not user.first():
+    if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Invalid Credentials")
     return user
